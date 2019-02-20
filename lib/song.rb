918f2acaf5e6ee3a42d96d891e1060a63d58148f
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-20 16:54:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,11 @@ require 'pry'
 
 class Song
   
-  extend Memorable
-  include Paramable 
+  # extend Memorable
+  # include Paramable 
+  
+extend Memorable::ClassMethods
+include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
