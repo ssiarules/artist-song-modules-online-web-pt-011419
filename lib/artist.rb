ef31672c7a96ad50b9fc9203440ddd3ea3c94cc2
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-20 17:14:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class Artist 
   
-  # extend Memorable
-  # include Paramable 
+  extend Memorable
+  include Paramable 
   
 extend Memorable::ClassMethods
 include Memorable::InstanceMethods
